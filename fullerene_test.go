0b01795af708e92ade95d4507c5dffb8a7a7d926
yu81 <usui1118@yu81.jp>
2016-10-19 13:58:54 +0900

    Add tests.

--- fullerene_test.go
+++ fullerene_test.go
@@ -69,4 +69,84 @@ func TestFullerene_IsBirthday(t *testing.T) {
 	assert.False(t, frLeap.IsBirthday(Date(2015, 3, 1, 0, 0, 0, 0, &emptyLocation), true))
 	assert.False(t, frLeap.IsBirthday(Date(2016, 3, 1, 0, 0, 0, 0, &emptyLocation), true))
 	assert.False(t, frLeap.IsBirthday(Date(2016, 3, 1, 0, 0, 0, 0, &emptyLocation), false))
+	tm := time.Now()
+	tm.Weekday()
+	fr.isBirthdayEx(frLeap, false)
+}
+
+func TestFullerene_Age(t *testing.T) {
+	type Case struct {
+		Birthday    Fullerene
+		ExpectedAge int
+	}
+	targetDate := Date(2016, 10, 2, 0, 0, 0, 0, &time.Location{})
+	tests := []Case{
+		{Date(2015, 1, 1, 0, 0, 0, 0, &time.Location{}), 1},
+		{Date(2014, 12, 31, 0, 0, 0, 0, &time.Location{}), 1},
+		{Date(1999, 7, 16, 0, 0, 0, 0, &time.Location{}), 17},
+		{Date(1988, 2, 29, 0, 0, 0, 0, &time.Location{}), 28},
+		{Date(1988, 3, 3, 0, 0, 0, 0, &time.Location{}), 28},
+		{Date(1988, 10, 1, 0, 0, 0, 0, &time.Location{}), 27},
+		{Date(1988, 10, 2, 0, 0, 0, 0, &time.Location{}), 28},
+		{Date(1988, 10, 3, 0, 0, 0, 0, &time.Location{}), 28},
+	}
+
+	for _, d := range tests {
+		assert.Equal(t, d.ExpectedAge, d.Birthday.Age(targetDate), d.Birthday.String())
+	}
+}
+
+func BenchmarkFullerene_Date(b *testing.B) {
+	location := &time.Location{}
+	for i := 0; i < b.N; i++ {
+		Date(2016, 1, 1, 0, 0, 0, 0, location)
+	}
+}
+
+func BenchmarkFullerene_Date2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Date(2016, 1, 1, 0, 0, 0, 0, &time.Location{})
+	}
+}
+
+func BenchmarkFullerene_Date3(b *testing.B) {
+	b.StopTimer()
+	fr := Date(2016, 1, 1, 0, 0, 0, 0, &time.Location{})
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		_ = fr.Year()
+	}
+}
+
+func BenchmarkFullerene_Date4(b *testing.B) {
+	b.StopTimer()
+	fr := Date(2016, 1, 1, 0, 0, 0, 0, &time.Location{})
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		_, _ = fr.Month(), fr.Day()
+	}
+}
+
+func BenchmarkFullerene_Date5(b *testing.B) {
+	b.StopTimer()
+	fr := Date(2016, 1, 1, 0, 0, 0, 0, &time.Location{})
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		_, _, _ = fr.Date()
+	}
+}
+
+func Benchmark_Date(b *testing.B) {
+	b.StopTimer()
+	location := &time.Location{}
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		time.Date(2016, 1, 1, 0, 0, 0, 0, location)
+	}
+}
+
+func BenchmarkFullerene_Age(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Date(2015, 1, 1, 0, 0, 0, 0, &time.Location{})
+	}
 }
